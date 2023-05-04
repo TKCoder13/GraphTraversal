af8c636bfe1465185c437c24a28ab1f149f59ee6
TKCoder13 <TKCoding13@gmail.com>
2023-05-03 22:03:44 -0700

    completed DFT and BFT

--- GraphTraversal.java
+++ GraphTraversal.java
@@ -5,20 +5,47 @@ import java.util.LinkedList;
 
 public class GraphTraversal {
     
+    
     // -- graph as an adjacency list
-    ArrayList adjacencyList[];
-    // -- graph as an adjacency matrix data structure
-    // public int[][] adjacencyMatrix = {
-    //     {0, 1, 0, 0, 0, 0, 0, 0, 0},
-    //     {1, 0, 1, 0, 0, 0, 0, 1, 0},
-    //     {0, 1, 0, 0, 0, 0, 0, 1, 0},
-    //     {0, 0, 0, 0, 0, 0, 0, 1, 1},
-    //     {0, 0, 0, 0, 0, 1, 1, 0, 0},
-    //     {0, 0, 0, 0, 1, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 1, 0, 0, 1, 0},
-    //     {0, 1, 1, 1, 0, 0, 1, 0, 0},
-    //     {0, 0, 0, 1, 0, 0, 0, 0, 0}
-    // };
+    int[][] aList = {
+        {2, 4},
+        {1, 5, 7},
+        {4, 6, 7},
+        {1, 3, 5},
+        {2, 4, 6, 7},
+        {3, 5},
+        {2, 3, 5}
+    };
+
+    int[][] bList = {
+        {4, 5},
+        {3, 4},
+        {2, 4},
+        {1, 2, 3, 7},
+        {1, 6},
+        {5, 7},
+        {4, 6}
+    };
+
+    int[][] cList = {
+        {2, 4, 5},
+        {3, 4, 7},
+        {4},
+        {6, 7},
+        {4},
+        {5},
+        {6}
+    };
+
+    int[][] dList = {
+        {2, 4, 5},
+        {3},
+        {7},
+        {2, 3, 6, 7},
+        {4},
+        {7},
+        {}
+    };
 
     public int[][] a_adjacencyMatrix = {
        /*1  2  3  4  5  6  7 */
@@ -62,36 +89,37 @@ public class GraphTraversal {
          {0, 0, 0, 1, 0, 0, 0},/*5*/
          {0, 0, 0, 0, 0, 0, 1},/*6*/
          {0, 0, 0, 0, 0, 0, 0} /*7*/
-     };
+    };
 
-    public ArrayList[] matrix2List(int[][] matrix) {
 
-        ArrayList[] list = new ArrayList[matrix.length];
-        for (int i = 0; i < list.length; i++) {
-            list[i] = new ArrayList();
-        }
-
-        for (int v = 0; v < matrix.length; v++) {
-            for (int w = 0; w < matrix[v].length; w++) {
-                if (matrix[v][w] == 1) {
-                    list[v].add(w);
-                }
+    public int[][] list2Matrix(int[][] list) {
+        int[][] array = {
+            /*1  2  3  4  5  6  7 */
+             {0, 0, 0, 0, 0, 0, 0},/*1*/
+             {0, 0, 0, 0, 0, 0, 0},/*2*/
+             {0, 0, 0, 0, 0, 0, 0},/*3*/
+             {0, 0, 0, 0, 0, 0, 0},/*4*/
+             {0, 0, 0, 0, 0, 0, 0},/*5*/
+             {0, 0, 0, 0, 0, 0, 0},/*6*/
+             {0, 0, 0, 0, 0, 0, 0} /*7*/
+         };
+        
+        for (int row = 0; row < list.length; row++) {
+            for (int i = 0; i < list[row].length; i++) {
+                array[row][list[row][i]-1] = 1;
             }
         }
-        for (int v = 0; v < matrix.length; v++) {
-            Collections.sort(list[v]);
-        }
-        return list;
+        return array;
     }
 
     // -- keep track of nodes that have been visited
     public int[] marks = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    Queue<Integer> queue = new LinkedList<Integer>();
+    LinkedList<Integer> queue = new LinkedList<Integer>();
 
     // -- visitation is just printing the node being visited
     public void visit(int v) {
-        System.out.println("Visit: " + v);
+        System.out.print((v+1));
     }
 
     // -- mark node v as visited
@@ -122,40 +150,28 @@ public class GraphTraversal {
         }
     }
 
-    // -- perform the Depth First Traversal using an adjList
-    public void DepthFirstTraversal(ArrayList[] graph, int v) {
-        // -- visit node v
-        visit(v);
-        // -- visit node v as visited
-        mark(v);
-        // -- for all edges vw in graph
-        for (int i = 0; i < graph[v].size(); i++) {
-            int w = (int) graph[v].get(i);
-        // -- this is for greatest to least
-        // -- for (int w = graph[v].size - 1; w >= 0; w--) {} 
-            // -- if w is not marked
-            if (marks[w] == 0) {
-                // -- recursive call from node w
-                DepthFirstTraversal(graph, w);
-            }
+    public void enqueue(int v) { 
+        queue.addLast(v);
+    }
+
+    public int dequeue() { 
+        return queue.pop();
+    }
+
+    public void reset() {
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = 0;
         }
     }
 
-    public void enqueue(int v) {
-        queue.add(v);
-    }
-
-    public void dequeue() {
-        queue.poll();
-    }
     public void BreadthFirstTraversal(int[][] graph, int v) {
         visit(v);
         mark(v);
         enqueue(v);
         while (queue.peek() != null) {
-            dequeue();
+            int popped = dequeue();
             for (int w = 0; w < graph.length; w++) {
-                if (marks[w] == 0) {
+                if (marks[w] == 0 && graph[popped][w] > 0) {
                     visit(w);
                     mark(w);
                     enqueue(w);
@@ -164,24 +180,69 @@ public class GraphTraversal {
         }
     }
 
-    public void BreadthFirstTraversal(ArrayList[] graph, int v) {
-        visit(v);
-        mark(v);
-        enqueue(v);
-        while (queue.peek() != null) {
-            dequeue();
-            for (int w = 0; w < graph[v].size(); w++) {
-                if (marks[w] == 0) {
-                    visit(w);
-                    mark(w);
-                    enqueue(w);
-                }
-            }
-        }
-    }
     public static void main(String args[]) {
         GraphTraversal traversal = new GraphTraversal();
-        traversal.adjacencyList = traversal.matrix2List(traversal.a_adjacencyMatrix);
-        traversal.BreadthFirstTraversal(traversal.adjacencyList, 0);
+        
+        System.out.println("Depth First Traversals (Adjacency Matrix):");
+        System.out.print("Graph A: ");
+        traversal.DepthFirstTraversal(traversal.a_adjacencyMatrix, 0);
+        traversal.reset();
+        System.out.print("\nGraph B: ");
+        traversal.DepthFirstTraversal(traversal.b_adjacencyMatrix, 0);
+        traversal.reset();
+        System.out.print("\nGraph C: ");
+        traversal.DepthFirstTraversal(traversal.c_adjacencyMatrix, 0);
+        traversal.reset();
+        System.out.print("\nGraph D: ");
+        traversal.DepthFirstTraversal(traversal.d_adjacencyMatrix, 0);
+        traversal.reset();
+
+        System.out.println("\nBreadth First Traversals (Adjacency Matrix):");
+        System.out.print("Graph A: ");
+        traversal.BreadthFirstTraversal(traversal.a_adjacencyMatrix, 0);
+        traversal.reset();
+        System.out.print("\nGraph B: ");
+        traversal.BreadthFirstTraversal(traversal.b_adjacencyMatrix, 0);
+        traversal.reset();
+        System.out.print("\nGraph C: ");
+        traversal.BreadthFirstTraversal(traversal.c_adjacencyMatrix, 0);
+        traversal.reset();
+        System.out.print("\nGraph D: ");
+        traversal.BreadthFirstTraversal(traversal.d_adjacencyMatrix, 0);
+        traversal.reset();
+        
+        // -- conversion from Lists to Matrices
+        int[][] aMat = traversal.list2Matrix(traversal.aList);
+        int[][] bMat = traversal.list2Matrix(traversal.bList);
+        int[][] cMat = traversal.list2Matrix(traversal.cList);
+        int[][] dMat = traversal.list2Matrix(traversal.dList);
+
+        System.out.println("\n\nDepth First Traversals (Adjacency List):");
+        System.out.print("Graph A: ");
+        traversal.DepthFirstTraversal(aMat, 0);
+        traversal.reset();
+        System.out.print("\nGraph B: ");
+        traversal.DepthFirstTraversal(bMat, 0);
+        traversal.reset();
+        System.out.print("\nGraph C: ");
+        traversal.DepthFirstTraversal(cMat, 0);
+        traversal.reset();
+        System.out.print("\nGraph D: ");
+        traversal.DepthFirstTraversal(dMat, 0);
+        traversal.reset();
+
+        System.out.println("\nBreadth First Traversals (Adjacency List):");
+        System.out.print("Graph A: ");
+        traversal.BreadthFirstTraversal(aMat, 0);
+        traversal.reset();
+        System.out.print("\nGraph B: ");
+        traversal.BreadthFirstTraversal(bMat, 0);
+        traversal.reset();
+        System.out.print("\nGraph C: ");
+        traversal.BreadthFirstTraversal(cMat, 0);
+        traversal.reset();
+        System.out.print("\nGraph D: ");
+        traversal.BreadthFirstTraversal(dMat, 0);
+        traversal.reset();
     }
 }
